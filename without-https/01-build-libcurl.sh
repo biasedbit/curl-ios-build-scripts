5b81a6f0ecc5d071906841bab9e010bbff53d49b
Bruno de Carvalho <bruno@biasedbit.com>
2012-07-14 06:13:33 +0100

    Modified non-https build script to use latest xcode
    No longer dumping output to logs (makes it easier to see when stuff goes wrong)
    Dropped armv6 support

--- without-https/01-build-libcurl.sh
+++ without-https/01-build-libcurl.sh
@@ -8,14 +8,14 @@ ARCHIVE="${LIBNAME}-${VERSION}.tar.gz"
 SDK="5.1"
 
 # Enabled/disabled protocols (the fewer, the smaller the final binary size)
-export PROTOCOLS="--enable-http --disable-rtsp --disable-ftp --disable-file --disable-ldap --disable-ldaps"
-PROTOCOLS="${PROTOCOLS} --disable-rtsp --disable-dict --disable-telnet --disable-tftp"
-PROTOCOLS="${PROTOCOLS} --disable-pop3 --disable-imap --disable-smtp --disable-gopher"
+PROTOCOLS="--enable-http --disable-rtsp --disable-ftp --disable-file --disable-ldap --disable-ldaps \
+           --disable-rtsp --disable-dict --disable-telnet --disable-tftp \
+           --disable-pop3 --disable-imap --disable-smtp --disable-gopher"
 
 CONFIGURE_FLAGS="--without-ssl --without-libssh2 --without-ca-bundle ${PROTOCOLS}"
 
 DIR=`pwd`
-ARCHS="i386 armv6 armv7"
+ARCHS="i386 armv7"
 
 
 # Download or use existing tar.gz
@@ -32,10 +32,7 @@ fi
 # Create out dirs
 mkdir -p "${DIR}/bin"
 mkdir -p "${DIR}/lib"
-mkdir -p "${DIR}/lib-i386"
-mkdir -p "${DIR}/lib-no-i386"
 mkdir -p "${DIR}/src"
-mkdir -p "${DIR}/log"
 
 
 # Build for all archs
@@ -48,7 +45,8 @@ do
         PLATFORM="iPhoneOS"
     fi
 
-    echo "Building ${LIBNAME} ${VERSION} for ${PLATFORM} ${SDK} ${ARCH}..."
+    echo ""
+    echo "* Building ${LIBNAME} ${VERSION} for ${PLATFORM} ${SDK} ${ARCH}..."
 
     # Expand source code, prepare output directory and set log
     tar zxf ${ARCHIVE} -C "${DIR}/src"
@@ -56,14 +54,12 @@ do
     mv -f "${DIR}/src/curl-${VERSION}" "${DIR}/src/${LIBNAME}-${VERSION}"
 
     mkdir -p "${DIR}/bin/${LIBNAME}-${VERSION}/${PLATFORM}${SDK}-${ARCH}"
-    LOG="${DIR}/log/${LIBNAME}-${VERSION}-${PLATFORM}${SDK}-${ARCH}.log"
 
     cd "${DIR}/src/${LIBNAME}-${VERSION}"
 
-    # pre Xcode 4.3 DEVROOT
-    # export DEVROOT="/Developer/Platforms/${PLATFORM}.platform/Developer"
-    export DEVROOT="/Applications/Xcode.app/Contents/Developer/Platforms/${PLATFORM}.platform/Developer"
-    export SDKROOT="${DEVROOT}/SDKs/${PLATFORM}${SDK}.sdk"
+    XCODE=$(xcode-select --print-path)
+    DEVROOT="${XCODE}/Platforms/${PLATFORM}.platform/Developer"
+    SDKROOT="${DEVROOT}/SDKs/${PLATFORM}${SDK}.sdk"
     export CC="${DEVROOT}/usr/bin/llvm-gcc-4.2"
     export LD="${DEVROOT}/usr/bin/ld"
     export CPP="${DEVROOT}/usr/bin/llvm-cpp-4.2"
@@ -77,30 +73,29 @@ do
     export CXXFLAGS="-arch ${ARCH} -pipe -no-cpp-precomp -isysroot ${SDKROOT} -I${DIR}/include"
 
     ./configure --host=${ARCH}-apple-darwin --disable-shared --enable-static ${CONFIGURE_FLAGS} \
-                --prefix="${DIR}/bin/${LIBNAME}-${VERSION}/${PLATFORM}${SDK}-${ARCH}" >> "${LOG}" 2>&1
+                --prefix="${DIR}/bin/${LIBNAME}-${VERSION}/${PLATFORM}${SDK}-${ARCH}"
 
-    make >> "${LOG}" 2>&1
-    make install >> "${LOG}" 2>&1
+    make
+    make install
     cd ${DIR}
     rm -rf "${DIR}/src/${LIBNAME}-${VERSION}"
 done
 
 
 # Create a single .a file for all architectures
-echo "Creating binaries for ${LIBNAME}..."
+echo ""
+echo "* Creating binaries for ${LIBNAME}..."
 lipo -create "${DIR}/bin/${LIBNAME}-${VERSION}/iPhoneSimulator${SDK}-i386/lib/${LIBNAME}.a" \
-             "${DIR}/bin/${LIBNAME}-${VERSION}/iPhoneOS${SDK}-armv6/lib/${LIBNAME}.a" \
              "${DIR}/bin/${LIBNAME}-${VERSION}/iPhoneOS${SDK}-armv7/lib/${LIBNAME}.a" \
      -output "${DIR}/lib/${LIBNAME}.a"
 
 # Create a single .a file for all arm architectures
-lipo -create "${DIR}/bin/${LIBNAME}-${VERSION}/iPhoneOS${SDK}-armv6/lib/${LIBNAME}.a" \
-             "${DIR}/bin/${LIBNAME}-${VERSION}/iPhoneOS${SDK}-armv7/lib/${LIBNAME}.a" \
-     -output "${DIR}/lib-no-i386/${LIBNAME}.a"
+lipo -create "${DIR}/bin/${LIBNAME}-${VERSION}/iPhoneOS${SDK}-armv7/lib/${LIBNAME}.a" \
+     -output "${DIR}/lib/${LIBNAME}-armv7.a"
 
 # Create a single .a file for i386
 lipo -create "${DIR}/bin/${LIBNAME}-${VERSION}/iPhoneSimulator${SDK}-i386/lib/${LIBNAME}.a" \
-     -output "${DIR}/lib-i386/${LIBNAME}.a"
+     -output "${DIR}/lib/${LIBNAME}-i386.a"
 
 
 # Copy the header files to include
@@ -115,4 +110,5 @@ fi
 cp -R "${DIR}/bin/${LIBNAME}-${VERSION}/${PLATFORM}${SDK}-${FIRST_ARCH}/include/" \
       "${DIR}/include/${LIBNAME}/"
 
-echo "Finished; ${LIBNAME} binary created for archs: ${ARCHS}"
+echo ""
+echo "* Finished; ${LIBNAME} binary created for archs: ${ARCHS}"
