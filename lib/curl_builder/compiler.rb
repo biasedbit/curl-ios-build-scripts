2df30fbece642f48a7e336359baa389e64f823fe
Berend Ozceri <berend@eye.fi>
2014-03-20 20:32:47 -0700

    Allow setting the OS X SDK to "none" to allow x86_64 to be an iOS build.

--- lib/curl_builder/compiler.rb
+++ lib/curl_builder/compiler.rb
@@ -57,7 +57,7 @@ module CurlBuilder
     def platform_for(architecture)
       case architecture
       when "x86_64"
-        "MacOSX"
+        setup(:osx_sdk_version) == "none" ? "iPhoneSimulator" : "MacOSX"
       when "i386"
         "iPhoneSimulator"
       else
